3617be1511e94e885b9d94de5270351afcb8fe32
gokul-sreeni <gokulsreeni26@gmail.com>
2025-01-23 22:09:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+const navbar = document.querySelector('.navbar');
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 50) {
+    navbar.classList.add('scrolled');
+  } else {
+    navbar.classList.remove('scrolled');
+  }
+});
+
+document.querySelectorAll('.nav-links a').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    const targetId = this.getAttribute('href').substring(1);
+    const targetElement = document.getElementById(targetId);
+
+    window.scrollTo({
+      top: targetElement.offsetTop - navbar.offsetHeight,
+      behavior: 'smooth'
+    });
+  });
+});
